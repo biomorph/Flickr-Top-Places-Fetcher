4ef80570aa52f09f9c87c520f2afa1c1cd2fe91f
biomorph <ravi@server.com>
2012-07-26 16:03:28 -0600

    Initial Commit

---
+++ Flickr Top Places Fetcher/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Flickr Top Places Fetcher
+//
+//  Created by Ravi Alla on 7/26/12.
+//  Copyright (c) 2012 Ravi Alla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Flickr Top Places Fetcher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Flickr Top Places Fetcher
+//
+//  Created by Ravi Alla on 7/26/12.
+//  Copyright (c) 2012 Ravi Alla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
